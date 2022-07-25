6c2fa6f20912054823aa28e7246e29fa5d16cd6c
Mateus <mateusvm94@gmail.com>
2022-07-25 10:20:15 -0300

    Perguntas são finalizadas agora

--- src/Pergunta.js
+++ src/Pergunta.js
@@ -2,26 +2,43 @@ import React from 'react'
 import play_pergunta from './play_pergunta.png'
 import turn from './turn.png'
 import errado from './errado.png'
+import certo from './certo.png'
+import quase from './quase.png'
 import './Pergunta.css'
 
 export default function Pergunta({index, pergunta, resposta, feito, setFeito}){
-    const [select, setSelect] = React.useState(false);
-    const [flip, setFlip] = React.useState(false);
+    const [status, setStatus] = React.useState("pergunta");
+    const [content, setContent] = React.useState("Pergunta " +(index+1))
+    const [cor, setCor] = React.useState("#333333")
+    const [image, setImage] = React.useState("")
+    // const [select, setSelect] = React.useState(false);
+    // const [flip, setFlip] = React.useState(false);
     return(
-        <div className={select?(flip?"turned":"selected"):"pergunta"}>
-            <h1>{select?(flip?resposta:pergunta):"Pergunta "+ (index+1)}</h1>
-            <img className="play-button" src={play_pergunta} onClick={()=> setSelect(!select)}/>
-            <img className="turn-button" src={turn} onClick={()=> setFlip(!flip)}/>
-            <button className="red" onClick={()=> voltaPergunta()}>Não Lembrei</button>
-            <button className="yellow" onClick={()=> voltaPergunta()}>Quase não Lembrei</button>
-            <button className="green" onClick={()=> voltaPergunta()}>Zap!</button>
+        <div className={status} style={{color:cor}}>
+            <h1>{content}</h1>
+            <img className="play-button" src={play_pergunta} onClick={()=>(
+                setStatus("selected"),
+                setContent(pergunta)
+            )
+                }/>
+            <img className="turn-button" src={turn} onClick={()=> (
+                setStatus("turned"),
+                setContent(resposta)
+                )}/>
+            <button className="red" onClick={()=> voltaPergunta("#FF3030",errado)}>Não Lembrei</button>
+            <button className="yellow" onClick={()=> voltaPergunta("#FF922E", quase)}>Quase não Lembrei</button>
+            <button className="green" onClick={()=> voltaPergunta("#2FBE34", certo)}>Zap!</button>
+            <img className="check-button" src={image} />
         </div>
     )
 
-    function voltaPergunta(){
-        setFeito(feito+1)
-        setSelect(false);
-        setFlip(false)
+    function voltaPergunta(color, img){
+        setFeito(feito+1);
+        setStatus("finished");
+        setContent("Pergunta " +(index+1));
+        setCor(color);
+        setImage(img)
+
     }
 }
 
